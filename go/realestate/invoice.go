917a1627c7517b4fd776d94f675e0d98740ebbb3
Erik Unger <erik@erikunger.com>
2025-06-18 17:18:13 +0200

    added RealEstateInvoice

---
+++ go/realestate/invoice.go
@@ -0,0 +1,10 @@
+package realestate
+
+import "github.com/docvibe-ai/api/go/invoicing"
+
+type RealEstateInvoice struct {
+	invoicing.AccountingInvoice
+
+	Section35aAmounts []*Section35aInvoiceAmount `json:"section35a_amounts,omitempty"`
+	IdentifiedObjects []*Object                  `json:"identified_objects,omitempty"`
+}
