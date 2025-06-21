8c1417de0affaafc27e6e05a120c28c5e51e4f18
Erik Unger <erik@erikunger.com>
2025-06-21 15:42:24 +0200

    Change Invoice struct to embed a pointer to AccountingInvoice

--- go/realestate/invoice.go
+++ go/realestate/invoice.go
@@ -3,7 +3,7 @@ package realestate
 import "github.com/docvibe-ai/api/go/invoicing"
 
 type Invoice struct {
-	invoicing.AccountingInvoice
+	*invoicing.AccountingInvoice
 
 	Section35aAmounts []*Section35aInvoiceAmount `json:"section35a_amounts,omitempty"`
 	IdentifiedObjects []*Object                  `json:"identified_objects,omitempty"`
